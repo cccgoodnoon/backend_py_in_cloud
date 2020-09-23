9335a36bd1c3039fba2a305968db09fade980b5c
cccgoodnoon <cccmeiqi@163.com>
2020-09-23 13:06:11 +0800

    用户权限

--- app.py
+++ app.py
@@ -1108,6 +1108,6 @@ def read_node():
 
 
 if __name__ == '__main__':
-    # from werkzeug.contrib.fixers import ProxyFix
-    # app.wsgi_app = ProxyFix(app.wsgi_app)
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run()
